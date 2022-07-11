7d9ea2d7cc9a84810fc5f3b0aac9d4ddc9ff9391
Ashish Bhardwaj <ashish.bhardwaj@monash.edu>
2022-07-11 23:40:48 +1000

    header added

---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div className="bg-red-500 h-16 flex items-center justify-center">
+      <h1 className="font-bold text-2xl text-white">OPEN TABLE BOOKING</h1>
+    </div>
+  );
+};
+export default Header;
